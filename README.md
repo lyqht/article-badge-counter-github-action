daf01a334a90f81ec8ff4ae23707c86535c927b2
lyqht <estee.tsw@gmail.com>
2021-11-24 15:56:51 +0800

    set up project with composite action and script to get updated readme text

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # article-count-workflow
+
+This github action workflow helps you to create a badge like this
+
+[![Website](https://img.shields.io/website?label=technical%20blog📝&up_color=%23abcbca&up_message=16%20articles&url=https%3A%2F%2Festeetey.dev)](https://esteetey.dev)
+
+
+It builds upon the popular blog-post-workflow Github Action and reuses some code logic there to produce this Shields.io badge.
---
+++ articleCount.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+const core = require('@actions/core');
+const redirectLink = core.getInput('redirect_link')
+const num_articles = core.getInput('articles')
+const readmeData = fs.readFileSync(core.getInput('readme_path'), 'utf8');
+
+const buildUpdatedReadme = (prevContent) => {
+    // code reused from 
+    // https://github.com/gautamkrishnar/blog-post-workflow/blob/eea5d3ccc163c20d69adcbb0b6ca38dbcb532abd/blog-post-workflow.js
+    const tagNameInput = core.getInput('comment_tag_name');
+    const tagToLookFor = tagNameInput ? `<!-- ${tagNameInput}:START` : `<!-- ARTICLE_BADGE:START -->`;
+    const closingTag = tagNameInput ? `<!-- ${tagNameInput}:END` : `<!-- ARTICLE_BADGE:END -->`;
+    const tagNewlineFlag = core.getInput('tag_post_pre_newline') === 'true';
+    const startOfOpeningTagIndex = prevContent.indexOf(
+        `${tagToLookFor}START`,
+    );
+    const endOfOpeningTagIndex = prevContent.indexOf(
+        closingTag,
+        startOfOpeningTagIndex,
+    );
+    const startOfClosingTagIndex = prevContent.indexOf(
+        `${tagToLookFor}END`,
+        endOfOpeningTagIndex,
+    );
+    if (
+        startOfOpeningTagIndex === -1 ||
+        endOfOpeningTagIndex === -1 ||
+        startOfClosingTagIndex === -1
+    ) {
+        // Exit with error if comment is not found on the readme
+        core.error(
+            `Cannot find the comment tag on the readme:\n${tagToLookFor}:START -->\n${tagToLookFor}:END -->`
+        );
+        process.exit(1);
+    }
+
+    const newContent = `[![Website](https://img.shields.io/website?label=technical%20blog📝&up_color=%23abcbca&up_message=${num_articles}%20articles&url=https%3A%2F%2Festeetey.dev)](${redirectLink})`
+
+    return [
+        previousContent.slice(0, endOfOpeningTagIndex + closingTag.length),
+        tagNewlineFlag ? '\n' : '',
+        newContent,
+        tagNewlineFlag ? '\n' : '',
+        previousContent.slice(startOfClosingTagIndex),
+    ].join('');
+};
+
+const readmeData = fs.readFileSync(README_FILE_PATH, 'utf8');
+core.setOutput('updated_file', buildUpdatedReadme(readmeData))
