6de85114ac46b8a0e293747a43d92e4e145b5d12
lyqht <estee.tsw@gmail.com>
2021-12-03 12:35:40 +0800

    refactoring

--- articleCount.js
+++ articleCount.js
@@ -2,21 +2,17 @@ const fs = require('fs')
 const core = require('@actions/core');
 const path = require('path')
 
-const redirectLink = core.getInput("redirect_link")
-const num_articles = process.env.articles_length
-const readme_path = process.env.readme_path
-const readme_abs_path = path.join(process.env.GITHUB_WORKSPACE, readme_path)
-const readmeData = fs.readFileSync(readme_abs_path, 'utf8');
-
 const createBadge = () => {
     const style = core.getInput('badge_style')
-    const message_bg_color = core.getInput('badge_message_bg_color')
-
     const label = core.getInput('badge_label')
+    const message_bg_color = core.getInput('badge_message_bg_color')
     const message_suffix = core.getInput('badge_message_suffix')
+    const num_articles = process.env.articles_length
     const message = `${num_articles} ${message_suffix}`
-
+    
+    const redirectLink = core.getInput("redirect_link")
     const encodedURI = redirectLink ? encodeURI(redirectLink) : 'https%3A%2F%2Festeetey.dev' // need a valid query url even if user does not give a redirect link
+
     const badgeImgElement = `<img alt="Website" src="https://img.shields.io/website?label=${label}&up_message=${message}&url=${encodedURI}&style=${style}&up_color=${message_bg_color}"></img>`
     const badge = redirectLink
         ? `<a href="${redirectLink}">${badgeImgElement}</a>`
@@ -29,7 +25,6 @@ const buildUpdatedReadme = (prevContent) => {
     // code reused from 
     // https://github.com/gautamkrishnar/blog-post-workflow/blob/eea5d3ccc163c20d69adcbb0b6ca38dbcb532abd/blog-post-workflow.js
     const tagNameInput = core.getInput('comment_tag_name');
-    console.log({ tagNameInput })
     const tagToLookFor = tagNameInput ? `<!-- ${tagNameInput}:` : `<!-- ARTICLE_BADGE:`;
     const closingTag = "-->"
     const tagNewlineFlag = core.getInput('tag_post_pre_newline') === 'true';
@@ -64,6 +59,10 @@ const buildUpdatedReadme = (prevContent) => {
         prevContent.slice(startOfClosingTagIndex),
     ].join('');
 };
+
+const readme_path = process.env.readme_path
+const readme_abs_path = path.join(process.env.GITHUB_WORKSPACE, readme_path)
+const readmeData = fs.readFileSync(readme_abs_path, 'utf8');
 
 const newReadme = buildUpdatedReadme(readmeData);
 fs.writeFileSync(readme_abs_path, newReadme);
